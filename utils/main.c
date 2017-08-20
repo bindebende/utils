823835e167d851f35823de3b67c4bc667fadf036
Bindebende <hoffmann.bende@gmail.com>
2017-08-20 12:26:45 +0200

    utils

---
+++ utils/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  utils
+//
+//  Created by Bindebende on 2017. 08. 20..
+//  Copyright © 2017. Bendegúz Hoffmann. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
